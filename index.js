2160c3f60fab0b46a9007afd40906f38d28c5f68
anas11cs <annus.waheed01@gmail.com>
2021-03-22 18:02:48 +0500

    Final Commit-ADDED jwt token w/out session

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const connectDataBase= require('./configurations/dataBase')
 //Importing Routes from auth.js here
 const authRoute = require('./routes/auth');
-
+const postRoute = require('./routes/posts')
 // Database Connection
 connectDataBase();
 
@@ -11,5 +11,6 @@ app.use(express.json());
 
 // Route MiddleWare
 app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
 
 app.listen((3000), ()=> console.log('Server Up and Running'));
